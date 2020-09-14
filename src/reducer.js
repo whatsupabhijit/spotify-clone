2373d3b90b6cad2f8aa2509d342b4b78300bf1fc
Abhijit Dutta <whatsupabhijit@gmail.com>
2020-09-14 18:14:06 +0000

    Created React Context API component in DataLayer.js and created reducer component to listen to dispatch commands and take action in reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // token: 'bebee3b2e6e846dfb6552a2a2ec4f6b6',
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
