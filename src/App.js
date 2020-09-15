acd17d7ec2d25d175cd575ccd76f704033ee8a85
Abhijit Dutta <whatsupabhijit@gmail.com>
2020-09-15 10:24:26 +0000

    Grabbed the Token from Spotify API and put into React Context API i.e. Data Layer. Made Player screen visible if valid token is present

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Player from "./components/Player";
+import { useDataLayerValue } from "./DataLayer";
+import { getTokenFromUrl } from "./spotify";
+
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <Player />
-    </div>
-  );
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist("0BjTmuRKZW4eYSIzUQrnlv").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
+    }
+  }, [user, dispatch]);
+
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
